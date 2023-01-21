0fd5c0b64a0b99847ee5873f5dfd3e25fce0de63
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-01-21 03:59:46 -0500

    Completed all tests

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,13 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+  const newArray = [];
+  for(let i = 0; i < array.length; i++){
+    newArray.push(array[i] + '!')
+  }
+  return newArray;
+}
 
 /**
  * shoutWhileLoop()
@@ -28,7 +34,18 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  const newArray = [];
+  
+  let i = 0;
+
+  while(i < array.length){
+    newArray.push(array[i] + '!')
+    i++
+  }
+
+  return newArray;
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +61,16 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+const newArray = [];
+
+for(i of array){
+  newArray.push(i + '!')
+}
+
+return newArray;
+
+}
 
 /**
  * sumArray()
@@ -57,7 +83,15 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0;
+
+  for(let i = 0; i < array.length; i++){
+    sum = array[i] + sum
+  }
+
+  return sum;
+}
 
 /**
  * oddArray()
@@ -70,7 +104,17 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let newArray = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 !== 0){
+      newArray.push(array[i])
+    }
+  }
+
+  return newArray;
+}
 
 /**
  * evenArray()
@@ -83,7 +127,17 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let newArray = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      newArray.push(array[i])
+    }
+  }
+
+  return newArray;
+}
 
 /**
  * findSmallest()
@@ -96,7 +150,17 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  let curSmallNum = array[0];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < curSmallNum){
+      curSmallNum = array[i];
+    }
+  }
+
+  return curSmallNum;
+}
 
 /**
  * findLargest()
@@ -109,7 +173,17 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  let curLargeNum = array[0];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > curLargeNum){
+      curLargeNum = array[i];
+    }
+  }
+
+  return curLargeNum;
+}
 
 /**
  * findEqual()
@@ -126,7 +200,15 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === selected){
+      return true;
+    }
+  }
+  return false;
+}
 
 /**
  * removeDuplicates()
@@ -143,7 +225,12 @@ function findEqual(array, selected) {}
  *  //> [ 1, 11, 2, 3, 4, 9 ]
  */
 
-function removeDuplicates(array) {}
+function removeDuplicates(array) {
+
+let duplicate = [...new Set(array)]
+
+return duplicate;
+}
 
 // Do not change any code below this line.
 module.exports = {
--- src/09-word-problems.js
+++ src/09-word-problems.js
@@ -19,7 +19,34 @@
  *  applyDiscount(1000, 9, true);
  *  //> 700
  */
-function applyDiscount(priceInCents, age, hasMembership) {}
+function applyDiscount(priceInCents, age, hasMembership) {
+  let discount = 0;
+  
+  if(hasMembership && age >= 65){
+      discount = (priceInCents * 0.30);
+      priceInCents -= discount;
+      return priceInCents;
+  }
+  
+  if(hasMembership && age <= 10){
+      discount = (priceInCents * 0.30);
+      priceInCents -= discount;
+      return priceInCents;
+  }
+  
+  if(hasMembership && age >= 10 && age <= 65){
+      discount = (priceInCents * 0.20);
+      priceInCents -= discount
+  }
+  
+  if(age <= 10 || age >= 65){
+      discount = (priceInCents * 0.10);
+      priceInCents -= discount
+  }
+  
+  return priceInCents;
+  
+  }
 
 /**
  * getCartTotal()
@@ -40,7 +67,16 @@ function applyDiscount(priceInCents, age, hasMembership) {}
     getCartTotal(cart);
  *  //> "$30.00"
  */
-function getCartTotal(products) {}
+function getCartTotal(products) {
+  let total = 0;
+    for(let i = 0; i < products.length; i++){
+        const priceInCents = products[i].priceInCents;
+        const quantity = products[i].quantity;
+          
+        total += (priceInCents * quantity);
+      }
+      return `$${(total/100).toFixed(2)}`; 
+  }
 
 /**
  * compareLocations()
@@ -80,7 +116,26 @@ function getCartTotal(products) {}
     compareLocations(address1, address2);
     //> "Same city."
  */
-function compareLocations(address1, address2) {}
+function compareLocations(address1, address2) {
+
+  if(address1.street === address2.street && address1.city === address2.city && address1.state === address2.state && address1.zip === address2.zip){
+    return 'Same building.';
+  }
+
+  if(address1.city === address2.city && address1.state === address2.state && address1.zip === address2.zip){
+  return 'Same city.'
+  }
+  
+  if(address1.state === address2.state){
+    return 'Same state.'
+  }
+
+
+  if(address1.city !== address2.city || address1.state !== address2.state){
+    return 'Addresses are not near each other.'
+  }
+
+}
 
 /**
  * gradeAssignments()
@@ -127,7 +182,38 @@ function compareLocations(address1, address2) {}
     //>   },
     //> ];
  */
-function gradeAssignments(assignments) {}
+function gradeAssignments(assignments) {
+
+
+for(let i = 0; i < assignments.length; i++){
+  if(assignments[i].kind === "PASS-FAIL"){
+    if(assignments[i].score.received === assignments[i].score.max){
+      assignments[i].status = 'PASSED';
+    }
+    else{
+      assignments[i].status = 'FAILED'
+    }
+  }
+  
+  if(assignments[i].kind === 'PERCENTAGE'){
+    let percentage = (((assignments[i].score.received / assignments[i].score.max) * 100).toFixed(1));
+    if(percentage >= 80.0){
+      assignments[i].status = `PASSED: ${percentage}%`;
+    }
+    else{
+      assignments[i].status = `FAILED: ${percentage}%`
+    }
+  }
+
+  if(assignments[i].kind !== 'PASS-FAIL' && assignments[i].kind !== 'PERCENTAGE'){
+    assignments[i].status = `SCORE: ${assignments[i].score.received}/${assignments[i].score.max}`;
+  }
+
+}
+
+return assignments;
+
+}
 
 /**
  * createLineOrder()
@@ -152,7 +238,23 @@ function gradeAssignments(assignments) {}
     createLineOrder(people);
     //> [ "Ray Anderson", "America Marsh", "Wade Carson", "Patience Patel" ]
  */
-function createLineOrder(people) {}
+    function createLineOrder(people) {
+      let array1 = [];
+      let array2 = [];
+        
+      for(let i = 0; i < people.length; i++){
+          if(people[i].hasMembership){
+            array1.push(people[i].name);
+          }
+          else{
+            array2.push(people[i].name);
+          }
+        }
+        
+        let array = [...array1, ...array2]
+      
+        return array;
+      }
 
 module.exports = {
   applyDiscount,
