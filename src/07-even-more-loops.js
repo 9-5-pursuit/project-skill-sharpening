7ad037d17b2b28f0e6515ba0755b370a74d016f4
Nigel Phillips <nigelphillips@pursuit.org>
2023-01-23 04:00:11 -0500

    2023/01/23 4:00am final commit

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,13 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+  let arr = []
+  for (var s of array) {
+    arr.push(s + '!')
+  }
+  return arr
+}
 
 /**
  * shoutWhileLoop()
@@ -28,7 +34,16 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+  let arr = []
+  count = 0
+  while (true) {
+    if (arr.length === array.length) break;
+    arr.push(array[count] + '!')
+    count ++
+  }
+  return arr
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +59,13 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+  let arr = []
+  for (var s of array) {
+    arr.push(s + '!')
+  }
+  return arr
+}
 
 /**
  * sumArray()
@@ -57,7 +78,9 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  return array.reduce((prev, next) => prev + next, 0)
+}
 
 /**
  * oddArray()
@@ -70,7 +93,9 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  return array.filter(item => item %2 === 1)
+}
 
 /**
  * evenArray()
@@ -83,7 +108,9 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  return array.filter(item => item %2 === 0)
+}
 
 /**
  * findSmallest()
@@ -96,7 +123,11 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  return array.reduce((first, max) => {
+    return max > first ? first : max
+}, array[0])
+}
 
 /**
  * findLargest()
@@ -109,7 +140,11 @@ function findSmallest(array) {}
  *  findLargest([ 0, 11, -2, 5 ]);
  *  //> 11
  */
-function findLargest(array) {}
+function findLargest(array) {
+  return array.reduce((first, max) => {
+    return max > first ? max : first
+}, array[0])
+}
 
 /**
  * findEqual()
@@ -126,7 +161,9 @@ function findLargest(array) {}
  *  findEqual([ 0, 11, -2, 5 ], 9);
  *  //> false
  */
-function findEqual(array, selected) {}
+function findEqual(array, selected) {
+  return array.includes(selected)
+}
 
 /**
  * removeDuplicates()
